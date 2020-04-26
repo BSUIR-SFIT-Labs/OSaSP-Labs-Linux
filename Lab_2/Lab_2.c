49631bbec190edfeb00732d2eb35275af0005ade
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-04-26 16:08:37 +0300

    Fix a bug of incorrect indentation in the code

--- Lab_2/Lab_2.c
+++ Lab_2/Lab_2.c
@@ -12,132 +12,133 @@ char *AppName;
 
 int ProcessFolder(FILE *outfile, char *path)
 {
-	DIR *curDir;
-	curDir = opendir(path);
+    DIR *curDir;
+    curDir = opendir(path);
 
-	if (curDir == NULL)
-	{
-		fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
-		errno = 0;
+    if (curDir == NULL)
+    {
+        fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
+        errno = 0;
 
-		return -1;
-	}
+        return -1;
+    }
 
-	struct dirent *dent;
-	struct stat buf;
-	char *file = alloca(strlen(path) + NAME_MAX + 2);
+    struct dirent *dent;
+    struct stat buf;
+    char *file = alloca(strlen(path) + NAME_MAX + 2);
 
-	if (file==NULL)
-	{
-		fprintf(stderr,"%s: %s.", AppName, strerror(errno));
+    if (file==NULL)
+    {
+        fprintf(stderr,"%s: %s.", AppName, strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	long int sum = 0;
-	int count = 0;
-	int maxSize = -1;
-	char *maxFile = alloca(NAME_MAX);
+    long int sum = 0;
+    int count = 0;
+    int maxSize = -1;
+    char *maxFile = alloca(NAME_MAX);
 
-	if (maxFile==NULL)
-	{
-		fprintf(stderr, "%s: %s.", AppName, strerror(errno));
+    if (maxFile==NULL)
+    {
+        fprintf(stderr, "%s: %s.", AppName, strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	maxFile[0] = 0;
-	errno = 0;
+    maxFile[0] = 0;
+    errno = 0;
 
-	while (dent = readdir(curDir))
-	{
-		if (strcmp(".", dent->d_name) && strcmp("..", dent->d_name))
-		{
-			strcpy(file, path);
-			strcat(file, "/");
- 			strcat(file, dent->d_name);
+    while (dent = readdir(curDir))
+    {
+        if (strcmp(".", dent->d_name) && strcmp("..", dent->d_name))
+        {
+            strcpy(file, path);
+            strcat(file, "/");
+            strcat(file, dent->d_name);
 
-			if (lstat(file,&buf) == -1) 
-			{
-				fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
+            if (lstat(file,&buf) == -1) 
+            {
+                fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
 
-				return -1;
-			}
+                return -1;
+            }
 
-			if (!S_ISDIR(buf.st_mode))
-			{
-				if (buf.st_size > maxSize)
-				{
-					maxSize = buf.st_size;
-					strcpy(maxFile, basename(file));
-				}
+            if (!S_ISDIR(buf.st_mode))
+            {
+                if (buf.st_size > maxSize)
+                {
+                    maxSize = buf.st_size;
+                    strcpy(maxFile, basename(file));
+                }
 
-				sum += buf.st_size;
-				count++;
-			}
+                sum += buf.st_size;
+                count++;
+            }
 
-			if (S_ISDIR(buf.st_mode))
-			{
-				ProcessFolder(outfile, file);
-			}
-		}
-	}
+            if (S_ISDIR(buf.st_mode))
+            {
+                ProcessFolder(outfile, file);
+            }
+        }
+    }
 
-	if (errno != 0)
-	{
-		fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
+    if (errno != 0)
+    {
+        fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	printf("%s %d %ld %s\n", path, count, sum, maxFile);
-	fprintf(outfile, "%s %d %ld %s\n", path, count, sum, maxFile);
+    printf("%s %d %ld %s\n", path, count, sum, maxFile);
+    fprintf(outfile, "%s %d %ld %s\n", path, count, sum, maxFile);
 
-	if (closedir(curDir) == -1)
-	{
-		fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
+    if (closedir(curDir) == -1)
+    {
+        fprintf(stderr, "%s: %s %s\n", AppName, path, strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
-int main(int argc, char **argv){
-	AppName = alloca(strlen(basename(argv[0])));
-	strcpy(AppName, basename(argv[0]));
+int main(int argc, char **argv)
+{
+    AppName = alloca(strlen(basename(argv[0])));
+    strcpy(AppName, basename(argv[0]));
 
-	if (argc < 3)
-	{
-		fprintf(stderr, "%s: Enter two arguments.\n", AppName);
+    if (argc < 3)
+    {
+        fprintf(stderr, "%s: Enter two arguments.\n", AppName);
 
-		return -1;
-	}
+        return -1;
+    }
 
-	FILE *file;
+    FILE *file;
 
-	if ((file = fopen(argv[2], "w")) == NULL)
-	{
-		fprintf(stderr, "%s: %s %s\n", AppName, argv[2], strerror(errno));
+    if ((file = fopen(argv[2], "w")) == NULL)
+    {
+        fprintf(stderr, "%s: %s %s\n", AppName, argv[2], strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	if (realpath(argv[1], NULL) == NULL) 
-	{
-		fprintf(stderr, "%s: %s %s\n", AppName, argv[1], strerror(errno));
+    if (realpath(argv[1], NULL) == NULL) 
+    {
+        fprintf(stderr, "%s: %s %s\n", AppName, argv[1], strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	ProcessFolder(file, realpath(argv[1], NULL));
+    ProcessFolder(file, realpath(argv[1], NULL));
 
-	if (fclose(file) != 0)
-	{
-		fprintf(stderr,"%s: %s %s\n", AppName, argv[2], strerror(errno));
+    if (fclose(file) != 0)
+    {
+        fprintf(stderr,"%s: %s %s\n", AppName, argv[2], strerror(errno));
 
-		return -1;
-	}
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
